6b8877720a0b3db7111b1916c91ef0abc6e78dc3
M. Moet <PS250444@summacollege.nl>
2025-05-08 17:18:04 +0200

    Revert "Merge branch 'main'"
    This reverts commit b31c5e6de61e8fbb4628c02e55d7e0560d93658e, reversing
    changes made to 823bcd16d0fed49fe94e77d6040d5e0fdfee4711.

---
+++ Scripts/Slime.gd
@@ -0,0 +1,62 @@
+extends Node2D
+
+const SPEED = 200
+const GRAVITY = 900.0
+const MAX_FALL_SPEED = 1200.0
+
+var direction = 1
+var HP = 3
+var velocity: Vector2 = Vector2.ZERO
+
+const player_character = preload("res://Scripts/PlayerCharacter.gd")
+
+@onready var raycast_front: RayCast2D = $RaycastFront
+@onready var ray_cast_down: RayCast2D = $RayCastDown 
+@onready var gravity_ray: RayCast2D = $Grav 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var hitbox: Area2D = $Hitbox
+
+func _ready() -> void:
+	hitbox.connect("body_entered", self.take_damage)
+
+func _process(delta: float) -> void:
+	var collider_right = raycast_front.get_collider()
+
+	# Gravity simulation using Grav raycast
+	if !gravity_ray.is_colliding():
+		velocity.y += GRAVITY * delta
+		velocity.y = min(velocity.y, MAX_FALL_SPEED)
+	else:
+		velocity.y = 0
+
+	# Apply movement
+	position.x += direction * SPEED * delta
+	position.y += velocity.y * delta
+
+	# Direction change logic (uses ray_cast_down)
+	if !ray_cast_down.is_colliding():
+		toggle_direction()
+	if raycast_front.is_colliding() and !collider_right.is_in_group("player"):
+		toggle_direction()
+
+func toggle_direction():
+	direction *= -1
+	animated_sprite.flip_h = !animated_sprite.flip_h
+	ray_cast_down.position.x *= -1
+	raycast_front.position.x *= -1
+
+func take_damage(amount):
+	if amount is RigidBody2D:
+		amount.queue_free()  # Destroy the projectile that hit us
+
+	HP -= amount.damage
+	animated_sprite.modulate = Color(1, 0, 0)  # Red on hit
+	await get_tree().create_timer(0.2).timeout
+	if HP <= 0:
+		die()
+	animated_sprite.modulate = Color(1, 1, 1)
+	print("Enemy HP:", HP)
+
+
+func die():
+	queue_free()
